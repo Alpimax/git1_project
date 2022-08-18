10e211bace1300ef91052d20432569c11285d048
Alpimax <alperrozkann@gmail.com>
2022-08-18 10:43:49 -0400

    Added new things

--- src/Day_1_Git/HelloWorld.java
+++ src/Day_1_Git/HelloWorld.java
@@ -3,5 +3,10 @@ package Day_1_Git;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("good old day");
+
+        System.out.println("Added new things.");
+        System.out.println("Alper OZKAN");
+
+
     }
 }
