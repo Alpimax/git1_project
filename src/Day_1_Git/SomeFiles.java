cff30b929bed6f43dbfe1bfe6c8fc3594c29f005
Alpimax <alperrozkann@gmail.com>
2022-08-18 12:03:57 -0400

    Some files updated

--- src/Day_1_Git/SomeFiles.java
+++ src/Day_1_Git/SomeFiles.java
@@ -3,5 +3,7 @@ package Day_1_Git;
 public class SomeFiles {
     public static void main(String[] args) {
         System.out.println( "Some files.");
+
+        System.out.println("Some files updated ");
     }
 }
